beac1a48914fd107786de4d9021e7b982b1a7ad0
Shikhar Sharma <shikhar.sharma.53@gmail.com>
2022-12-10 12:08:57 +0530

    Eighth day part two

--- 2022/eighth-question/solution.js
+++ 2022/eighth-question/solution.js
@@ -9,83 +9,95 @@ const checkVisibleTrees = (input) => {
             visible[[a,b]] = false
         }
     }
+
+    const scenicScore = {}
+
     for (let i = 0; i < input.length; i++) {
         for(let j = 0; j < input[i].length; j++) {
                 const current = input[i][j]
+                let countOfTreesVisibleOnLeft = 0;
+                let countOfTreesVisibleOnRight = 0;
+                let countOfTreesVisibleOnTop = 0;
+                let countOfTreesVisibleOnBottom = 0;
+
+                let atCorner = 1;
+
                 if(i == 0 || i == input.length-1 || j == 0 || j == input[i].length-1) {
                     visible[[i,j]] = true   
+                    atCorner = 0
                 }
 
 
-                if(visible[[i,j]] == false) {
-                    let allLeftElementsAreSmaller = true
-                    for(let k = j-1; k >= 0; k--) {
-                        if(input[i][k] >= current) {
-                            allLeftElementsAreSmaller = false
-                            break
-                        }
-                    }
-                    if(allLeftElementsAreSmaller) {
-                        visible[[i,j]] = true
-                    }
-                }   
-
-                if(visible[[i,j]] == false) {
-                    
-                    let allRightElementsAreSmaller = true
-                    for(let k = j+1; k < input[i].length; k++) {
-                        if(input[i][k] >= current) {
-                            allRightElementsAreSmaller = false
-                            break
-                        }
-                    }
-                    if(allRightElementsAreSmaller) {
-                        visible[[i,j]] = true
+                let allLeftElementsAreSmaller = true
+                
+                for(let k = j-1; k >= 0; k--) {
+                    countOfTreesVisibleOnLeft++
+                    if(input[i][k] >= current) {
+                        allLeftElementsAreSmaller = false
+                        break
                     }
                 }
+                if(allLeftElementsAreSmaller) {
+                    visible[[i,j]] = true
+                } 
 
-                if(visible[[i,j]] == false) {
-                    let allTopElementsAreSmaller = true
-                    for(let k = i-1; k >= 0; k--) {
-                        if(input[k][j] >= current) {
-                            allTopElementsAreSmaller = false
-                            break
-                        }
-                    }
-                    if(allTopElementsAreSmaller) {
-                        visible[[i,j]] = true
-                    }
-                }  
 
-                // check bottom
-                if(visible[[i,j]] == false) {
-                    let allBottomElementsAreSmaller = true
-                    for(let k = i+1; k < input.length; k++) {
-                        if(input[k][j] >= current) {
-                            allBottomElementsAreSmaller = false
-                            break
-                        }
-                    }
-                    if(allBottomElementsAreSmaller) {
-                        visible[[i,j]] = true
+                let allRightElementsAreSmaller = true
+                for(let k = j+1; k < input[i].length; k++) {
+                    countOfTreesVisibleOnRight++
+                    if(input[i][k] >= current) {
+                        allRightElementsAreSmaller = false
+                        break
                     }
                 }
+                if(allRightElementsAreSmaller) {
+                    visible[[i,j]] = true
+                }
+
+                let allTopElementsAreSmaller = true
+                for(let k = i-1; k >= 0; k--) {
+                    countOfTreesVisibleOnTop++
+                    if(input[k][j] >= current) {
+                        allTopElementsAreSmaller = false
+                        break
+                    }
+                }
+                if(allTopElementsAreSmaller) {
+                    visible[[i,j]] = true
+                }
+
+
+                let allBottomElementsAreSmaller = true
+                for(let k = i+1; k < input.length; k++) {
+                    countOfTreesVisibleOnBottom++
+                    if(input[k][j] >= current) {
+                        allBottomElementsAreSmaller = false
+                        break
+                    }
+                }
+                if(allBottomElementsAreSmaller) {
+                    visible[[i,j]] = true
+                }
+
+                scenicScore[[i,j]] = countOfTreesVisibleOnLeft * countOfTreesVisibleOnRight * countOfTreesVisibleOnTop * countOfTreesVisibleOnBottom * atCorner
+                }
+
             }
-            }
-            
-            return visible
+
+            return scenicScore
 }
         
-    
 
+let keyWithTheHighestValue = 0
+let maxValue = 0
 
-let countTrueValues = 0
-Object.values(checkVisibleTrees(input)).forEach((value) => {
-    if(value == true) {
-        countTrueValues++
+Object.entries(checkVisibleTrees(input)).forEach(([key, value]) => {
+    if(value > maxValue) {
+        keyWithTheHighestValue = key
+        maxValue = value
     }
 })
 
-console.log(countTrueValues)
+console.log(maxValue)
 
 // console.log(input)
